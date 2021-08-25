8c0585f7b41d8b3d05715b7f69c78e938e4c7372
BillyThePhysicist <billydebenham@gmail.com>
2021-08-25 12:59:35 -0500

    Added error self testing methods and error catching to data analysis

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -8,22 +8,25 @@ import scipy.signal as spsi
 import globalVariables as gv
 import time
 import numba
+import warnings
 
 #FUTURE PHYSICISTS, THIS USES OBJECT ORIENTED PROGRAMMING.
 #Written by William Debenham(Huntington), billydebenham@gmail.com, 6197870499. Feel free to contact
 
 
-#TODO: CHANGE FITFUNC TO IT'S OWN COMMENTED FUNCTION
 
+def fit_Spectral_Data(imageFreqArr, imageSignalArr, peakMode='multi', lensHeating=False,vTMaxLens=10.0
+                             ,gammaMin=gv.LiTau/1E6,laserJitter=0.0):
+    dataAnalyzerObject=_DataAnalyzer()
+    dataAnalyzerObject.fit_Spectral_Profile(imageFreqArr,imageSignalArr,peakMode,lensHeating,vTMaxLens,gammaMin,laserJitter)
 
-class DataAnalyzer:
+class _DataAnalyzer:
     def __init__(self):
-        self.params=None #params for the current solution of fit_Spectral_Profile. contains [v0,a,b,sigma,gamma]
+        self._params=None #params for the current solution of fit_Spectral_Profile. contains [v0,a,b,sigma,gamma]
         #where v0: center frequency, a: height, b: offset from zero, sigma: standard deviation of temperature
         #disitribution, gamma: FWHM of natural linewidth plus other lorentzian broadening effects
-
-
-
+        self.vTMaxLens=None
+        self.gammaMin=None#provided
         self.T=None #temperature, kelvin
         self.F0=None #center frequency of the profile
         self.fitPeak=None #to keep track of peak value of fit. This is necesary because more or less sampling can cause
@@ -35,8 +38,7 @@ class DataAnalyzer:
         self.laserJitter=None #laser jitter, MHz
         self.imageFreqArr=None #1D array of frequency values corresponding to each image
         self.imageSignalArr=None #1D array of means or sums of image regions of interest
-    def fit_Spectral_Profile(self,imageFreqArr, imageSignalArr, peakMode='multi', lensHeating=False,vTMaxLens=10.0
-                             ,gamma=gv.LiTau/1E6,laserJitter=0.0):
+    def fit_Spectral_Profile(self,imageFreqArr, imageSignalArr, peakMode, lensHeating,vTMaxLens,gammaMin,laserJitter):
         #Fit spectral data. Can be (single peak or multipeak) and include the velocity distribution of lens.
         #imageFreqArr: frequency, or anything else, scale
         #imageSignalArr: Flourescence signal
@@ -48,8 +50,7 @@ class DataAnalyzer:
         #value, or allow to go above depending on gammaFloor
         #laserJitter: Jitter of laser, standard deviation, MHz.
 
-        if peakMode!= 'multi' and peakMode!='single':
-            raise Exception('Invalid peak mode. Choose multipeak or single peak')
+
         if lensHeating==False:
             freqTMaxLens=None
         else:
@@ -58,10 +59,11 @@ class DataAnalyzer:
         self.freqTMaxLens=freqTMaxLens
         self.peakMode=peakMode
         self.laserJitter=laserJitter
-
+        self.vTMaxLens=vTMaxLens
+        self.gammaMin=gammaMin
         self.imageFreqArr=imageFreqArr
         self.imageSignalArr=imageSignalArr
-
+        self.catch_Errors_And_Give_Warnings()
 
         def minimize(params):
             #wrapper function to be minimized. One cannot do least squares for the curve fit because if the lensHeating
@@ -76,33 +78,50 @@ class DataAnalyzer:
         aGuess=np.max(self.imageSignalArr)-np.min(self.imageSignalArr)
         bGuess=np.min(self.imageSignalArr)
 
-        bounds=[(F0Guess-deltaF,F0Guess+deltaF),(0.0,aGuess*2),(-2*np.abs(bGuess),2*np.abs(bGuess)),(0,30),(gamma,30)]
+        bounds=[(F0Guess-deltaF,F0Guess+deltaF),(0.0,aGuess*2),(-2*np.abs(bGuess),2*np.abs(bGuess)),(0,30),(gammaMin,30)]
 
         np.random.seed(42) #seed the generator to make results repeatable
         sol=spo.differential_evolution(minimize,bounds,polish=False) #a type of genetic algorithm, very robust.
 
         np.random.seed(int(time.time())) #resead to make pseudorandom
-        self.params=sol.x
-        self.T=self._calculate_Temp(self.params[3])
-        self.F0=self.params[0]
+        self._params=sol.x
+        self.T=self._calculate_Temp(self._params[3])
+        self.F0=self._params[0]
         self.fitPeak=self.spectral_Fit(imageFreqArr).max()
 
-        # print(self.params)
+        # print(self._params)
         # print(bounds)
         # plt.grid()
         # plt.plot(imageFreqArr,imageSignalArr)
         # plt.plot(imageFreqArr,self.spectral_Fit(imageFreqArr))
         # plt.show()
-        return self.params
+    def catch_Errors_And_Give_Warnings(self):
+        if self.peakMode!='multi' and self.peakMode!='single':
+            raise Exception('Invalid peak mode. Choose \'multi\' or \'single\'')
+        if not 100.0>self.gammaMin>=0.0:
+            raise Exception('Gamma must be in MHz and positive and it appears not to be')
+        if self.gammaMin<gv.LiTau/1e6:
+            warnings.warn("Gamma is set to a value below the natural linewidth")
+        if not 100.0>self.laserJitter>=0.0:
+            raise Exception('Laser jitter must be in MHz and positive and it appears not to be')
+        maxReasonableVTransLen=20.0
+        if not maxReasonableVTransLen>self.vTMaxLens>=0.0:
+            raise Exception('Transverse particle velocity leaving lens is set to a value that does not make sense. \n'
+                            'must be m/s, positive and less than a reasonably sized value')
+        maxExpectedFreqInMHz=10_000
+        if np.max(np.abs(self.imageFreqArr))>1e6*maxExpectedFreqInMHz:
+            raise Exception('It appears that the provided frequency data is not in MHz')
+        if isinstance(self.imageFreqArr, np.ndarray)==False or isinstance(self.imageSignalArr, np.ndarray)==False:
+            raise Exception('Frequency data and signal data must be numpy arrays')
 
     def spectral_Fit(self,v):
         #the function that returns the fit to the data provided in self.fit_Spectral_Profile.
         #v: frequency, MHz
         #return: The "signal", in arbitrary units
 
-        if self.params is None:
+        if self._params is None:
             raise Exception("You have not fit any data yet. You must call fit_Spectral_Profile first")
-        return self._spectral_Profile(v,*self.params,self.freqTMaxLens,self.peakMode,self.laserJitter)
+        return self._spectral_Profile(v,*self._params,self.freqTMaxLens,self.peakMode,self.laserJitter)
     def _spectral_Profile(self,v,v0,a,b,sigma,gamma,vTMaxLens,peakMode,laserJitter):
         #private method that returns the spectral profile. It may be convoluted with the geometric "heating" and or
         #contain multiple peaks
@@ -154,8 +173,7 @@ class DataAnalyzer:
         #---------SINGLE VOIGT FIT---------
         #this voigt is normalized to a height of 1, then multiplied by the variable a
         #there is no yaxis offset in the voigt, that is done later
-    @staticmethod
-    def _voigt(f, f0, a, sigma, gamma):
+    def _voigt(self,f, f0, a, sigma, gamma):
         #units must be consistent!!
         #f, frequency value
         #f0, center frequency
@@ -209,4 +227,44 @@ class DataAnalyzer:
     def calculate_Temp(self,sigma,MHz=True):
         return self._calculate_Temp(sigma,MHz=MHz)
 
+    def self_Test(self):
 
+        #_spectral_Profile(self,v,v0,a,b,sigma,gamma,vTMaxLens,peakMode,laserJitter)
+        numTestDataPoints=1000
+        testDataFreq=np.linspace(-100,100,num=numTestDataPoints)
+
+        testDataSignal=500.0*np.exp(-testDataFreq**2/100)+1000.0
+        np.random.seed(42) #set the seed to repeatable noise
+        testDataSignal+=30.0*np.random.random(numTestDataPoints) #add gaussion noise
+        np.random.seed(int(time.time())) #reseed the random generator
+
+        tester1=_DataAnalyzer()
+        tester1.fit_Spectral_Profile(testDataFreq,testDataSignal,'multi',False,10.0,gv.LiTau/1e6,0.0)
+        tester2=_DataAnalyzer()
+        tester2.fit_Spectral_Profile(testDataFreq,testDataSignal,'single',False,10.0,gv.LiTau/1e6,0.0)
+        tester3=_DataAnalyzer()
+        tester3.fit_Spectral_Profile(testDataFreq,testDataSignal,'multi',True,10.0,gv.LiTau/1e6,0.0)
+        tester4=_DataAnalyzer()
+        tester4.fit_Spectral_Profile(testDataFreq,testDataSignal,'single',True,10.0,gv.LiTau/1e6,0.0)
+        tester5=_DataAnalyzer()
+        tester5.fit_Spectral_Profile(testDataFreq,testDataSignal,'single',True,10.0,gv.LiTau/1e6,1.0)
+        #now test that saved results match with current results
+        # data=[]
+        # data.append([*tester1._params,tester1.T])
+        # data.append([*tester2._params,tester2.T])
+        # data.append([*tester3._params,tester3.T])
+        # data.append([*tester4._params,tester4.T])
+        # data.append([*tester5._params,tester5.T])
+        # np.savetxt('DataAnalyzer_TestData',np.asarray(data))
+        testData=np.loadtxt('DataAnalyzer_TestData')
+        assert np.all(testData[0]==np.asarray([*tester1._params,tester1.T]))
+        assert np.all(testData[1]==np.asarray([*tester2._params,tester2.T]))
+        assert np.all(testData[2]==np.asarray([*tester3._params,tester3.T]))
+        assert np.all(testData[3]==np.asarray([*tester4._params,tester4.T]))
+        assert np.all(testData[4]==np.asarray([*tester5._params,tester5.T]))
+        print('self test passed')
+def perform_Test():
+    tester=_DataAnalyzer()
+    tester.self_Test()
+
+perform_Test()
